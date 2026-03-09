7abd477c252f7c85781415012402ce36ece42594
zhangxiaominzi <110907165+zhangxiaominzi@users.noreply.github.com>
2026-03-09 11:26:12 +0800

    remove old code and add new files

--- calculate_LCMVweight.m
+++
@@ -1,9 +0,0 @@
-function [w,mu] = calculate_LCMVweight(L_j, Msnr, ratio)
-
-% 
-N_channels = size(Msnr, 1);
-C = cov(Msnr');  % 
-mu = ratio * trace(C) / N_channels;  
-C_reg = C + mu * eye(N_channels);
-w = (L_j' / C_reg * L_j) \ (L_j' / C_reg);
-
